b61897db15c56d96834d25c79712acae4fea7bb9
voidrlm <varjun609@gmail.com>
2025-03-23 14:17:44 -0400

    Changes in color scheme

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,35 @@
+import { createVuetify } from "vuetify";
+
+export default createVuetify({
+  theme: {
+    defaultTheme: "light",
+    themes: {
+      dark: {
+        dark: true,
+        colors: {
+          background: "#121212",
+          surface: "#2A004E",
+          primary: "#BB86FC",
+          secondary: "#03DAC6",
+          error: "#CF6679",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FB8C00",
+        },
+      },
+      light: {
+        dark: false,
+        colors: {
+          background: "#FFFFFF",
+          surface: "#FFFFFF",
+          primary: "#c00",
+          secondary: "#03DAC6",
+          error: "#B00020",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FB8C00",
+        },
+      },
+    },
+  },
+});
