7005b037df8bd5507ed08946f7ee50b03942d49a
voidrlm <varjun609@gmail.com>
2025-03-24 09:29:39 -0400

    Added end points

--- src/resources/api-endpoints.js
+++ src/resources/api-endpoints.js
@@ -1,17 +1,17 @@
 // Rest API EndPoints
 export const restAPIEndPoints = {
-  // Sample Authentication APIs
-  // Function: User login, Type: POST
-  login: { url: '/sample/auth/login', method: 'POST', isPublic: true },
-  // // Sample Organization APIs
-  // // Function: Fetch organization details, Type: GET
-  // fetchOrgDetails: {
-  //   url: '/sample/core/organization/',
-  //   method: 'GET',
-  // },
-  // // Function: Update organization details, Type: PUT
-  // updateOrgDetails: {
-  //   url: '/sample/core/organization/?organization_id={id}',
-  //   method: 'PUT',
-  // },
-}
+  chatbot: {
+    url: '/ask?question="{question}',
+    method: "GET",
+  },
+
+  contentMonitoring: {
+    url: "/check?content={content}",
+    method: "GET",
+  },
+
+  resumeAnalyse: {
+    url: "/resume?content={content}",
+    method: "GET",
+  },
+};
